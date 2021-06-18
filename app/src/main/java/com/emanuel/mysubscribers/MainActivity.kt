14444cc6290cb62f1c871f91844e7e6166bbbf72
Emanuel Rodrigues <emanuelrootsneite@gmail.com>
2021-06-18 14:25:12 -0300

    Configured toolbar to work with navHost and add style for the toolbar

--- app/src/main/java/com/emanuel/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/emanuel/mysubscribers/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.emanuel.mysubscribers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.emanuel.mysubscribers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Pegar a referência do navhost para poder pegar o seu navController
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.appToolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
